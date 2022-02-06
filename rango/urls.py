adde50e90474ba07f00bcdb5f7cf21d0a89433c5
swpinkerton <swpinkerton11@gmail.com>
2022-02-06 19:24:47 +0000

    task3

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,6 @@
 """tango_with_django_project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
 from rango import views
+
+app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango.
-    path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
 ]
-
